ac94f5525c2476497d2aebd9ac003139c99c5e49
cmput274 <cmput274@ualberta.ca>
2019-03-18 18:07:04 -0600

    final version

---
+++ client/build-mega2560/client.d
@@ -0,0 +1,24 @@
+build-mega2560/client.o: client.cpp \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino//variants/mega/pins_arduino.h \
+ /home/cmput274/arduino-ua/libraries/Adafruit_ILI9341/Adafruit_ILI9341.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Print.h \
+ /usr/share/arduino/libraries/SPI/SPI.h \
+ /home/cmput274/arduino-ua/libraries/Adafruit_GFX/Adafruit_GFX.h \
+ /home/cmput274/arduino-ua/libraries/Adafruit_GFX/gfxfont.h \
+ /usr/share/arduino/libraries/SD/SD.h \
+ /usr/share/arduino/libraries/SD/utility/SdFat.h \
+ /usr/share/arduino/libraries/SD/utility/Sd2Card.h \
+ /usr/share/arduino/libraries/SD/utility/Sd2PinMap.h \
+ /usr/share/arduino/libraries/SD/utility/SdInfo.h \
+ /usr/share/arduino/libraries/SD/utility/FatStructs.h \
+ /usr/share/arduino/libraries/SD/utility/SdFatUtil.h consts_and_types.h \
+ lcd_image.h map_drawing.h
